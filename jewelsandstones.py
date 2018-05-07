65843b97bbf904663c38e7f09ec15c174f250a7e
TimJLS <37449368+TimJLS@users.noreply.github.com>
2018-05-07 15:22:02 +0800

    Update jewelsandstones.py

--- jewelsandstones.py
+++ jewelsandstones.py
@@ -1,4 +1,3 @@
-
 class Solution:
     def numJewelsInStones(self, J, S):
         """
@@ -19,3 +18,17 @@ J = "aA"
 S = "aAAbbbb"
 ans = Solution()
 ans.numJewelsInStones(J,S)
+'''
+另一個我很喜歡的答案
+def jewels(S, J):
+    jewels = {x: 0 for x in J}
+    for char in S:
+        if char in jewels.keys():
+            jewels[char] += 1
+
+    return sum(jewels.values())
+
+J = "aZAb"
+S = "aAAizbbbbc"
+jewels(S,J)
+'''
